0d0135dfecf42bf0df0ed390db2f203d45601cf5
Noga Haskal <nogahaskal@gmail.com>
2020-12-06 14:27:23 +0200

    feat: add hastransfer request

--- src/transfer/transfer.repository.ts
+++ src/transfer/transfer.repository.ts
@@ -3,19 +3,24 @@ import { ITransfer } from './transfer.interface';
 import { transferModel } from './transfer.model';
 
 export class TransferRepository {
-    static async create(request: ITransfer) {
+    static async create(request: ITransfer): Promise<ITransfer> {
         if (!request) throw new ServerError();
         const transfer: ITransfer = await transferModel.create(request);
         return transfer;
     }
 
-    static async getMany(filter: Partial<ITransfer>) {
-        const transfers: ITransfer[] | [] = await transferModel.find(filter).exec();
+    static async getMany(filter: Partial<ITransfer>): Promise<ITransfer[]> {
+        const transfers: ITransfer[] = await transferModel.find(filter).exec();
         return transfers;
     }
 
-    static async getByID(id: string) {
+    static async getByID(id: string): Promise<ITransfer | null> {
         const transfer: ITransfer | null = await transferModel.findById(id).exec();
         return transfer;
+    }
+
+    static async exist(filter: Partial<ITransfer>): Promise<boolean> {
+        const exists: boolean = await transferModel.exists(filter);
+        return exists;
     }
 }
