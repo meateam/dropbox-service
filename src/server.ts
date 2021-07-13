baa4257d54af3df417fc7943be198b725ba018db
nettashviro <netta.zhviro@gmail.com>
2021-07-13 16:28:40 +0300

    fix healthcheck and mongo reconnection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import * as grpc from 'grpc';
 import * as protoLoader from '@grpc/proto-loader';
 import * as apm from 'elastic-apm-node';
-import { GrpcHealthCheck, HealthCheckResponse, HealthService } from 'grpc-ts-health-check';
 import { config } from './config';
 import { log, Severity } from './utils/logger';
 import { wrapper } from './utils/wrapper';
 import { DropboxMethods } from './dropbox/dropbox.methods';
+import {
+  GrpcHealthCheck,
+  HealthCheckRequest,
+  HealthCheckResponse,
+  HealthClient,
+  HealthService,
+} from 'grpc-ts-health-check';
 
 apm.start({
   serviceName: config.serviceName,
@@ -17,21 +23,27 @@ apm.start({
 export const serviceNames: string[] = ['', 'dropbox.dropboxService'];
 export const healthCheckStatusMap: any = {
   '': HealthCheckResponse.ServingStatus.UNKNOWN,
-  [config.serviceName]: HealthCheckResponse.ServingStatus.UNKNOWN
+  [config.serviceName]: HealthCheckResponse.ServingStatus.UNKNOWN,
 };
+const servicesNum = Object.keys(healthCheckStatusMap).length;
 
 export class Server {
-
   private dropbox_proto: any;
   public server: grpc.Server;
   public grpcHealthCheck: GrpcHealthCheck;
+  public requests: HealthCheckRequest[];
+  public healthClient: HealthClient;
 
   public constructor(address: string) {
     // Create the server
     this.server = new grpc.Server();
+    this.requests = Array<HealthCheckRequest>(servicesNum);
 
-    // Register the health service
+    // RegisterHealthService the health service
     this.grpcHealthCheck = new GrpcHealthCheck(healthCheckStatusMap);
+
+    // Create the health client
+    this.healthClient = new HealthClient(address, grpc.credentials.createInsecure());
 
     this.initiateProto();
     this.addServices();
@@ -45,8 +57,13 @@ export class Server {
     const DROPBOX_PROTO_PATH: string = './proto/dropbox/dropbox.proto';
 
     // Suggested options for similarity to existing grpc.load behavior
-    const dropboxPackageDefinition: protoLoader.PackageDefinition = protoLoader.loadSync(
-            DROPBOX_PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true });
+    const dropboxPackageDefinition: protoLoader.PackageDefinition = protoLoader.loadSync(DROPBOX_PROTO_PATH, {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true,
+    });
 
     // Has the full package hierarchy
     const dropboxProtoDescriptor: grpc.GrpcObject = grpc.loadPackageDefinition(dropboxPackageDefinition);
@@ -57,18 +74,28 @@ export class Server {
   private addServices() {
     this.server.addService(HealthService, this.grpcHealthCheck);
 
+    // Set services
+    for (const service in healthCheckStatusMap) {
+      const request = new HealthCheckRequest();
+      request.setService(service);
+      this.requests.push(request);
+    }
+
     const dropboxService = {
       CanApproveToUser: wrapper(DropboxMethods.CanApproveToUser),
       CreateRequest: wrapper(DropboxMethods.CreateRequest),
       HasTransfer: wrapper(DropboxMethods.HasTransfer),
       GetTransfersInfo: wrapper(DropboxMethods.GetTransfersInfo),
-      GetApproverInfo: wrapper(DropboxMethods.GetApproverInfo)
+      GetApproverInfo: wrapper(DropboxMethods.GetApproverInfo),
     };
 
     this.server.addService(this.dropbox_proto.Dropbox.service, dropboxService);
   }
 
-  public setHealthStatus(status: number): void {
-    serviceNames.forEach(serviceName => this.grpcHealthCheck.setStatus(serviceName, status));
+  setHealthStatus(status: HealthCheckResponse.ServingStatus): void {
+    this.requests.forEach((request) => {
+      const serviceName: string = request.getService();
+      healthCheckStatusMap[serviceName] = status;
+    });
   }
 }
--- src/utils/connectMongo.ts
+++ src/utils/connectMongo.ts
@@ -6,24 +6,20 @@ import { log, Severity } from './logger';
 import { getCurrTraceId } from './wrapper';
 
 export async function connectMongo(server: Server) {
-  const db = mongoose.connection;
   log(Severity.INFO, 'trying to mongo server', 'connectDB');
+  const db = mongoose.connection;
 
   try {
-    await mongoose.connect(config.mongo.connectionString, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    });
-
-    log(Severity.INFO, `connected to ${config.mongo.connectionString}`, 'connectDB');
-    server.setHealthStatus(HealthCheckResponse.ServingStatus.SERVING);
+    await startConnectionAttempts(server);
   } catch (err) {
     server.setHealthStatus(HealthCheckResponse.ServingStatus.NOT_SERVING);
     log(Severity.ERROR, `did not connect to mongo: ${err}`, 'connectDB');
   }
 
+  db.on('connected', () => {
+    log(Severity.INFO, `connected to ${config.mongo.connectionString}`, 'connectDB');
+    server.setHealthStatus(HealthCheckResponse.ServingStatus.SERVING);
+  });
   db.on('error', (err) => {
     log(Severity.ERROR, 'mongo connection error!', 'connectDB', getCurrTraceId(), err);
     server.setHealthStatus(HealthCheckResponse.ServingStatus.NOT_SERVING);
@@ -32,4 +28,66 @@ export async function connectMongo(server: Server) {
     log(Severity.ERROR, 'mongo disconnected', 'connectDB');
     server.setHealthStatus(HealthCheckResponse.ServingStatus.NOT_SERVING);
   });
+}
+
+/**
+ * Attempts to connect to mongo connectionRetries times.
+ * Waits reconnectTimeout ms bewteen attempts.
+ * @param server - the server trying to connect.
+ */
+async function startConnectionAttempts(server: Server) {
+  log(Severity.INFO, `connecting to ${config.mongo.connectionString}`, 'connectDB');
+
+  const retries = parseInt(config.mongo.connectionRetries, 10);
+  const timeout = parseInt(config.mongo.reconnectTimeout, 10);
+
+  for (let i = 1; i <= retries; i++) {
+    const connectionRes: { success: boolean; error: Error | null } = await connect();
+
+    if (!connectionRes.success) {
+      log(Severity.ERROR, `mongo connection retry (${i}/${retries})`, 'connectDB', getCurrTraceId(), {
+        errMsg: connectionRes.error?.message,
+        stack: connectionRes.error?.stack,
+      });
+
+      server.setHealthStatus(HealthCheckResponse.ServingStatus.NOT_SERVING);
+      await sleep(timeout);
+    } else {
+      log(Severity.INFO, `connected to ${config.mongo.connectionString}`, 'connectDB');
+      server.setHealthStatus(HealthCheckResponse.ServingStatus.SERVING);
+      break;
+    }
+  }
+}
+
+/**
+ * Connects to mongo with mongoConnectionString.
+ */
+async function connect(): Promise<{ success: boolean; error: Error | null }> {
+  let success: boolean = false;
+  let error: Error | null = null;
+
+  await mongoose.connect(
+    config.mongo.connectionString,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    },
+    async (err) => {
+      if (!err) {
+        success = true;
+      } else {
+        success = false;
+        error = err;
+      }
+    }
+  );
+
+  return { success, error };
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
